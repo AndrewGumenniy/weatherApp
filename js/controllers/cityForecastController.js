270b5c4515ebeabd0ddc467b7cc3464dd88589de
AndrewGumenniy <gumenniy.oik@gmail.com>
2017-11-17 12:44:50 +0200

    Update cityForecastController.js

--- js/controllers/cityForecastController.js
+++ js/controllers/cityForecastController.js
@@ -1,8 +1,28 @@
 weatherApp.controller('cityForecastController',
   ['$scope','$http','$routeParams', function ($scope,$http,$routeParams) {
-    // forecast request for id
-    var URL = 'https://api.openweathermap.org/data/2.5/forecast';
     $scope.item = $routeParams.itemId;
+    // forecast request for id for current data
+    var URL = 'https://api.openweathermap.org/data/2.5/weather';
+    var request = {
+      method: 'POST',
+      url: URL,
+      params: {
+        id: $scope.item,
+        mode: 'json',
+        units: 'metric',
+        appid: '37e8a116f937408c4043e9d7a993e7c3'
+      }
+    }
+    $http(request)
+    .then(function(response) { 
+      $scope.currentCityData = response.data;
+      console.log($scope.currentCityData);
+    }).
+    catch(function(response) {
+      $scope.currentCityData = response.data;
+    });
+    // forecast request for id for 5 days forecast
+    var URL = 'https://api.openweathermap.org/data/2.5/forecast';
     var request = {
       method: 'POST',
       url: URL,
@@ -31,7 +51,4 @@ weatherApp.controller('cityForecastController',
     $scope.isSet = function(tabNum){
       return $scope.tab === tabNum;
     };
-}]);
-
-
-  
+}]);  
